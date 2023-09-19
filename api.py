ef2db2720efadd018dd6bb8aef6b6155f8502b9b
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-09-19 19:28:24 -0300

    get, post and delete with the API

--- api.py
+++ api.py
@@ -5,13 +5,43 @@ app = Flask(__name__)
 
 api = Api(app)
 
-class HelloWorld(Resource):
+brands = []
 
+class BrandNames(Resource):
+
+    def get(self, name):
+
+        for brand in brands:
+            if brand['name'] == name:
+                return brand
+
+        return {'name':None}, 404
+
+    def post(self, name):
+
+        brand = {'name':name}
+
+        brands.append(brand)
+
+        return brand
+
+    def delete(self, name):
+
+        for index, brand in enumerate(brands):
+            if brand['name'] == name:
+                deleted_brand = brands.pop(index)
+
+                return {'note':'delete_success'}
+
+
+class AllBrands(Resource):
+    
     def get(self):
-        return {'hello':'world'}
+        return {'brands':brands}
 
+api.add_resource(BrandNames, '/brand/<string:name>')
+api.add_resource(AllBrands, '/brands')
 
-api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
